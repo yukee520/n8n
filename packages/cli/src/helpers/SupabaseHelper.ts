532c333571efe84d223376757831bf6accb6a877
yukee520 <yukee92tmc@hotmail.com>
2025-07-18 19:39:04 +0800

    Create SupabaseHelper.ts

---
+++ packages/cli/src/helpers/SupabaseHelper.ts
@@ -0,0 +1,7 @@
+// packages/cli/src/helpers/SupabaseHelper.ts
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
